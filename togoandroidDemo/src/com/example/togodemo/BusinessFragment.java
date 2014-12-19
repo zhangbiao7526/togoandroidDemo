a57bc081fa84e0c98200350dc624202a10442305
xibeiguyue <2118356023@qq.com>
2014-12-19 11:05:18 +0800

    wang

--- togoandroidDemo/src/com/example/togodemo/BusinessFragment.java
+++ togoandroidDemo/src/com/example/togodemo/BusinessFragment.java
@@ -34,22 +34,20 @@ public class BusinessFragment extends Fragment implements OnClickListener {
 	//创建TextView集合对象
 	private List<TextView> tv_list=new ArrayList<TextView>();
 	private List<TextView> tv_desclist=new ArrayList<TextView>();
+	private ImageView news_minprice_pic1;
+	//活动视图
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View newsLayout = inflater.inflate(R.layout.news_layout, container,
 				false);
 		fb=FinalBitmap.create(getActivity());
-		
+		//活动更多 按钮控件
 		btn_businessF_more1=(Button) newsLayout.findViewById(R.id.btn_more1);
 		btn_businessF_more2=(Button) newsLayout.findViewById(R.id.btn_more2);
 		btn_businessF_more3=(Button) newsLayout.findViewById(R.id.btn_more3);
-		
-		btn_businessF_more1.setOnClickListener(this);
-		btn_businessF_more2.setOnClickListener(this);
-		btn_businessF_more3.setOnClickListener(this);
-		
-		ImageView news_minprice_pic1=(ImageView) newsLayout.findViewById(R.id.news_minprice_pic1);
+		//活动图片和文本的控件
+		news_minprice_pic1=(ImageView) newsLayout.findViewById(R.id.news_minprice_pic1);
 		news_minprice_price_tv1=(TextView)newsLayout.findViewById(R.id.news_minprice_price_tv1);
 		news_minprice_price_tv2=(TextView)newsLayout.findViewById(R.id.news_minprice_price_tv2);
 		news_minprice_price_tv3=(TextView)newsLayout.findViewById(R.id.news_minprice_price_tv3);
@@ -57,7 +55,7 @@ public class BusinessFragment extends Fragment implements OnClickListener {
 		news_minprice_desc_tv1=(TextView)newsLayout.findViewById(R.id.news_minprice_desc_tv1);
 		news_minprice_desc_tv2=(TextView)newsLayout.findViewById(R.id.news_minprice_desc_tv2);
 		news_minprice_desc_tv3=(TextView)newsLayout.findViewById(R.id.news_minprice_desc_tv3);
-		
+		//文本视图集合
 		tv_list.add(news_minprice_price_tv1);
 		tv_list.add(news_minprice_price_tv2);
 		tv_list.add(news_minprice_price_tv3);
@@ -65,7 +63,12 @@ public class BusinessFragment extends Fragment implements OnClickListener {
 		tv_desclist.add(news_minprice_desc_tv1);
 		tv_desclist.add(news_minprice_desc_tv2);
 		tv_desclist.add(news_minprice_desc_tv3);
-		
+		//单击事件
+		btn_businessF_more1.setOnClickListener(this);
+		btn_businessF_more2.setOnClickListener(this);
+		btn_businessF_more3.setOnClickListener(this);
+		news_minprice_pic1.setOnClickListener(this);
+		//访问服务器调取活动要显示的文本信息
 		BusinessNet.getDataFromSQL(this,tv_list,tv_desclist);
 		//图片路径，并显示图片
 		String uri=VARIABLE.HOMEVIPAGER_URI6;
@@ -89,6 +92,8 @@ public class BusinessFragment extends Fragment implements OnClickListener {
 		}else if(id==R.id.btn_more3){
 			intent.setClass(getActivity(),Activity_SalesPromotion.class);
 			startActivity(intent);
+		}else if(id==R.id.news_minprice_pic1){
+			
 		}
 			
 	}
